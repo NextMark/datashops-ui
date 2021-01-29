6af1d8d83a600f2f55f7d87ad617ed23327a11e0
NextMark <qinshiweiy@gmail.com>
2021-01-29 19:28:34 +0800

    fix

--- src/view/job/jobInstance.vue
+++ src/view/job/jobInstance.vue
@@ -22,61 +22,65 @@
                 style="width: 100%"
                 tooltip-effect="dark"
         >
-            <el-table-column label="ID"
+            <el-table-column
                              type="index" fixed="left"
                              width="40"></el-table-column>
 
-            <el-table-column label="ID" prop="job.maskId" width="180"></el-table-column>
+            <el-table-column label="ID" prop="job.maskId" width="170"></el-table-column>
 
-            <el-table-column label="名称" prop="job.name" width="180"></el-table-column>
-
-            <el-table-column label="优先级" prop="job.priority" width="100"></el-table-column>
+            <el-table-column label="名称" width="180">
+                <template slot-scope="scope">
+                    <router-link :to="{path: '/layout/dataDevelop', query:{id: scope.row.id}}">
+                        {{scope.row.job.name}}
+                    </router-link>
+                </template>
+            </el-table-column>
 
             <el-table-column label="创建人" prop="job.owner" width="120"></el-table-column>
-            <el-table-column label="作业类型" width="80">
+            <el-table-column label="类型" width="60">
                 <template slot-scope="scope">
                     <svg class="icon-1-5" aria-hidden="true">
                         <use :xlink:href="scope.row.job.type |getJobIcon"></use>
                     </svg>
                 </template>
             </el-table-column>
-            <el-table-column label="资源" prop="job.queueId" width="120"></el-table-column>
+            <el-table-column label="资源" prop="job.queueId" width="100"></el-table-column>
 
-            <el-table-column label="调度周期" width="100">
+            <el-table-column label="调度周期" width="80">
                 <template slot-scope="scope">{{scope.row.job.schedulingPeriod|formatSchedulingPeriod}}</template>
             </el-table-column>
+            <el-table-column label="状态" width="100">
+                <template slot-scope="scope">
+                    <el-button type="primary" size="mini" plain>{{scope.row.state|formatState}}</el-button>
+                </template>
+            </el-table-column>
+            <el-table-column label="执行时长" width="150">
+                <template slot-scope="scope">{{scope.row.startTime|runDuration(scope.row.endTime)}}</template>
+            </el-table-column>
 
-            <el-table-column label="提交日期" width="180">
+            <el-table-column label="提交日期" width="160">
                 <template slot-scope="scope">{{scope.row.submitTime}}</template>
             </el-table-column>
-            <el-table-column label="执行日期" width="180">
+            <el-table-column label="执行日期" width="160">
                 <template slot-scope="scope">{{scope.row.startTime}}</template>
             </el-table-column>
-            <el-table-column label="结束日期" width="180">
+            <el-table-column label="结束日期" width="160">
                 <template slot-scope="scope">{{scope.row.endTime}}</template>
             </el-table-column>
-            <el-table-column label="按钮组" min-width="220" fixed="right">
+            <el-table-column label="按钮组">
                 <template slot-scope="scope">
                     <el-button
                             class="table-button"
                             @click="updateWorkflowProcess(scope.row)"
                             size="small"
                             type="primary"
-                    >手动执行</el-button>
+                    >重跑</el-button>
                     <el-button
                             class="table-button"
                             @click="viewWorkflowProcess(scope.row)"
                             size="small"
                             type="warning"
-                    >查看</el-button>
-                    <el-popover placement="top" width="160" v-model="scope.row.visible">
-                        <p>确定要删除吗？</p>
-                        <div style="text-align: right; margin: 0">
-                            <el-button size="mini" type="text" @click="scope.row.visible = false">取消</el-button>
-                            <el-button type="primary" size="mini" @click="deleteJobGraph(scope.row)">确定</el-button>
-                        </div>
-                        <el-button type="danger" icon="el-icon-delete" size="mini" slot="reference">删除</el-button>
-                    </el-popover>
+                    >日志</el-button>
                 </template>
             </el-table-column>
         </el-table>
@@ -101,7 +105,7 @@
     import infoList from "@/mixins/infoList";
 
     import { formatSchedulingPeriod, getJobIcon } from '@/utils/job';
-
+    var moment = require('moment');
 
     export default {
         name: "JobGraph",
@@ -117,7 +121,56 @@
         },
         filters: {
             formatSchedulingPeriod,
-            getJobIcon
+            getJobIcon,
+            formatState(state) {
+                if (state === 0) {
+                    return '已创建'
+                }
+                if (state === 1) {
+                    return '等待资源'
+                }
+                if (state === 2) {
+                    return '等待依赖'
+                }
+                if (state === 3) {
+                    return '等待执行'
+                }
+                if (state === 4) {
+                    return '执行中'
+                }
+                if (state === 5) {
+                    return '成功'
+                }
+                if (state === 6) {
+                    return '失败'
+                }
+                if (state === 7) {
+                    return '被杀'
+                }
+            },
+            runDuration: function(start, end) {
+                const dateDiff = moment(end).unix() - moment(start).unix()
+                if (dateDiff <= 1000) {
+                    return 0
+                }
+                const dayDiff = Math.floor(dateDiff / (24 * 3600));
+                const leave1 = dateDiff % (24 * 3600);
+                const hours = Math.floor(leave1 / (3600));
+                const leave2 = leave1 % 3600;
+                const minutes = Math.floor(leave2 / 60);
+                const leave3 = leave2 % 60;
+                const seconds = Math.round(leave3);
+                if (minutes === 0) {
+                    return seconds + ' 秒'
+                }
+                if (hours === 0) {
+                    return minutes + ' 分钟 ' + seconds + ' 秒'
+                }
+                if (dayDiff === 0) {
+                    return hours + ' 小时 ' + minutes + ' 分钟 ' + seconds + ' 秒'
+                }
+                return dayDiff + '天 ' + hours + ' 小时 ' + minutes + ' 分钟 ' + seconds + ' 秒'
+            },
         },
         methods: {
             //条件搜索前端看此方法
