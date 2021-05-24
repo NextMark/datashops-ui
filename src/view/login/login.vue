8528382f585937d801792436cb8295300f4f154b
NextMark <qinshiweiy@gmail.com>
2021-05-24 17:36:56 +0800

    fix

--- src/view/login/login.vue
+++ src/view/login/login.vue
@@ -3,7 +3,7 @@
     <div class="container">
       <div class="top">
         <div class="desc">
-          <img class="logo_login" src="@/assets/logo-touming.jpeg" alt="" />
+          <img class="logo_login" src="http://img.yyshuo.cn/ds/common/logo-touming.jpeg" alt="" />
         </div>
         <div class="header">
           <a href="/">
