1a19d779bc335566965803b4797b7899ea473974
NextMark <qinshiweiy@gmail.com>
2021-06-15 15:21:39 +0800

    fix

--- src/view/layout/github/index.vue
+++ src/view/layout/github/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="search-component">
     <div class="user-box">
-      <a href="https://github.com/NextMark/datashops-demo" target="_blank" class="el-icon-link search-icon"></a>
+      <a href="https://github.com/NextMark/datashops" target="_blank" class="el-icon-link search-icon"></a>
     </div>
   </div>
 </template>
